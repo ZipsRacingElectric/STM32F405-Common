f01b495e75ac3c7e646fc613f906553a81e264d8
Cole Barach <colebarach@tutanota.com>
2025-01-20 10:44:30 -0500

    Added a function for applying a low-pass filter to the pidController_t structure's deriviate term. The pidCalculate function now stores some intermediate information in the pidController_t structure, meaning antiwindup, derivitave filtering, and other improvements to the controller can now be implemented as additional steps following the base calculation.

--- src/controls/pid_controller.c
+++ src/controls/pid_controller.c
@@ -7,85 +7,89 @@
 float pidCalculate (pidController_t* pid, float y, float deltaTime)
 {
 	// PID Controller Output:
-	//   x(k) = kp * p(k) + ki * i(k) + kd * d(k)
+	//   x(k) = k_p * y_p(k) + k_i * y_i(k) + k_d * y_d(k)
 	//   where:
 	//     x(k) is the control variable (output value),
 	//     y(k) is the process variable (input value),
 	//     y_sp is the set-point (target value)
 	//
 	// Proportional Error Term:
-	//   p(k) = y_sp - y(k)
+	//   yp(k) = y_sp - y(k)
 	//
 	// Integral Right-Hand Riemann Sum Approximation:
-	//   i(k) = p(1) * (t(1) - t(0)) + p(2) * (t(2) - t(1)) + ... + p(k) * (t(k) - t(k-1))
-	//        = p(k) * (t(k) - t(k-1)) + i(k-1)
-	//        = p(k) * delta_t(k) + i(k-1)
+	//   yi(k) = yp(1) * (t(1) - t(0)) + yp(2) * (t(2) - t(1)) + ... + yp(k) * (t(k) - t(k-1))
+	//         = yp(k) * (t(k) - t(k-1)) + yi(k-1)
+	//         = yp(k) * t_delta(k) + yi(k-1)
+	//   where:
+	//     t_delta(k) = t(k) - t(k-1)
 	//
 	// Derivative Difference-Quotient Approximation:
-	//   d(k) = (p(k) - p(k-1)) / (t(k) - t(k-1))
-	//        = (p(k) - p(k-1)) / delta_t(k)
+	//   yd(k) = (yp(k) - yp(k-1)) / (t(k) - t(k-1))
+	//         = (yp(k) - yp(k-1)) / t_delta(k)
 
-	float p = pid->ySetPoint - y;
+	float yp = pid->ySetPoint - y;
 
-	float i = p * deltaTime + pid->iPrime;
-	pid->iPrime = i;
+	float yi = yp * deltaTime + pid->yiPrime;
+	pid->yiPrime = yi;
 
-	float d = (p - pid->dPrime) / deltaTime;
-	pid->dPrime = p;
+	float yd = (yp - pid->ypPrime) / deltaTime;
+	pid->ypPrime = yp;
+
+	pid->xp = pid->kp * yp;
+	pid->xi = pid->ki * yi;
+	pid->xd = pid->kd * yd;
+	pid->x = pid->xp + pid->xi + pid->xd;
 
 	// Prevent NaN propogation
-	if (pid->iPrime != pid->iPrime)
-		pid->iPrime = 0.0f;
-	if (pid->dPrime != pid->dPrime)
-		pid->dPrime = 0.0f;
+	if (isnan (pid->ypPrime) || isinf (pid->ypPrime))
+		pid->ypPrime = 0.0f;
+	if (isnan (pid->yiPrime) || isinf (pid->yiPrime))
+		pid->yiPrime = 0.0f;
 
-	return pid->kp * p + pid->ki * i + pid->kd * d;
+	return pid->x;
 }
 
-float pidAntiWindup (pidController_t* pid, float y, float deltaTime, float xMin, float xMax)
+float pidFilterDerivative (pidController_t* pid, float a, float* xdPrime)
+{
+	// Weighted Rolling Average Low-Pass Filter:
+	//   x_dOut(k) = (1 - a) * x_dIn(k) + a * x_dIn(k - 1)
+
+	// Calculate the new derivative term and store the value for the next iteration.
+	pid->xd = (1 - a) * pid->xd + a * (*xdPrime);
+	*xdPrime = pid->xd;
+
+	// Re-calculate the output after modifying the derivative.
+	pid->x = pid->xp + pid->xi + pid->xd;
+	return pid->x;
+}
+
+float pidApplyAntiWindup (pidController_t* pid, float xMinimum, float xMaximum)
 {
 	// Anti-windup is a technique applied to prevent integral terms for growing
 	// rapidly (running away) when the output value is fully saturated. The
 	// simplest approach is to back-calculate a value for the integral term
 	// that doesn't saturate the output.
 
-	// See above for PID calculation.
-
-	float p = pid->ySetPoint - y;
-
-	float i = p * deltaTime + pid->iPrime;
-	pid->iPrime = i;
-
-	float d = (p - pid->dPrime) / deltaTime;
-	pid->dPrime = p;
-
-	// Prevent NaN propogation
-	if (isnan (pid->iPrime) || isinf (pid->iPrime))
-		pid->iPrime = 0.0f;
-	if (isnan (pid->dPrime) || isinf (pid->dPrime))
-		pid->dPrime = 0.0f;
-
-	float nonIntegral = pid->kp * p + pid->kd * d;
-	float x = nonIntegral + pid->ki * i;
-
-	if (x > xMax)
+	if (pid->x > xMaximum)
 	{
+		// Back-calculate the integral term to de-saturate the output.
 		if (pid->ki != 0)
-			pid->iPrime = (xMax - nonIntegral) / pid->ki;
+			pid->yiPrime = (xMaximum - pid->xp - pid->xd) / pid->ki;
 		else
-			pid->iPrime = 0;
+			pid->yiPrime = 0;
 
-		x = xMax;
+		pid->x = xMaximum;
 	}
-	else if (x < xMin)
+	else if (pid->x < xMinimum)
 	{
+		// Back-calculate the integral term to de-saturate the output.
 		if (pid->ki != 0)
-			pid->iPrime = (xMin - nonIntegral) / pid->ki;
+			pid->yiPrime = (xMinimum - pid->xp - pid->xd) / pid->ki;
 		else
-			pid->iPrime = 0;
+			pid->yiPrime = 0;
 
-		x = xMin;
+		pid->x = xMinimum;
 	}
 
-	return x;
+	return pid->x;
 }
--- src/controls/pid_controller.h
+++ src/controls/pid_controller.h
@@ -12,13 +12,35 @@
 
 typedef struct
 {
+	/// @brief The proportional coefficient, in x-Units / y-Unit.
 	float kp;
+
+	/// @brief The integral coefficient, in x-Units / (y-Unit * seconds).
 	float ki;
+
+	/// @brief The derivative coefficient, in x-Units / (y-Units / second).
 	float kd;
 
+	/// @brief The set-point to target, in y-Units.
 	float ySetPoint;
-	float iPrime;
-	float dPrime;
+
+	/// @brief The previous proportional term, should be initialized to 0.
+	float ypPrime;
+
+	/// @brief The running integral term, should be initialized to 0.
+	float yiPrime;
+
+	/// @brief The output of the controller. Does not need initialized.
+	float x;
+
+	/// @brief The output contribution of the proportional term. Does not need initialized.
+	float xp;
+
+	/// @brief The output contribution of the integral term. Does not need initialized.
+	float xi;
+
+	/// @brief The output contribution of the derivative term. Does not need initialized.
+	float xd;
 } pidController_t;
 
 // Functions ------------------------------------------------------------------------------------------------------------------
@@ -27,21 +49,34 @@ typedef struct
  * @brief Calculates the output value to set according to the given PID controller.
  * @param pid The PID controller to use.
  * @param y The measured value of the process variable.
- * @param deltaTime The amount of time ellapsed since the last update (in seconds).
+ * @param deltaTime The amount of time elapsed since the last update (in seconds).
+ * filtering, 0.5 => 50/50 split between current input and previous input.
  * @return The output value to set.
  */
 float pidCalculate (pidController_t* pid, float y, float deltaTime);
 
 /**
- * @brief Calculates the output value to set according to the givent PID controller. Integral anti-windup is used to prevent
- * runaway when output is saturated.
+ * @brief Applies a low-pass filter to the derivative term of a PID controller's output.
+ * @note The PID controller should already have its output calculated via @c pidCalculate .
  * @param pid The PID controller to use.
- * @param y The measured value of the process variable.
- * @param deltaTime The amount of time ellapsed since the last update (in seconds).
- * @param xMin The minimum viable value of the output value.
- * @param xMax The maximum viable value of the output value.
- * @return The output value to set.
+ * @param a The measurment gain of the filter to apply, specifies how much of the output should consist of the current
+ * measurement vs. the previous measurements. (0 => no filtering, 0.5 => 50% current & 50% previous). Must be in the range
+ * [0, 1), not including 1.
+ * @param xdPrime Float storing the previous value of the derivative term, written to contain the current derivative term. Must
+ * initialized to 0 and preserved between iterations.
+ * @return The output value after filtering.
  */
-float pidAntiWindup (pidController_t* pid, float y, float deltaTime, float xMin, float xMax);
+float pidFilterDerivative (pidController_t* pid, float a, float* xdPrime);
+
+/**
+ * @brief Back-calculates a PID controller's integral term to de-saturate the output, if it is saturated. This is used to
+ * prevent integral runaway for a saturated system.
+ * @note The PID controller should already have its output calculated via @c pidCalculate .
+ * @param pid The PID controller to apply the anti-windup to.
+ * @param xMinimum The minimum viable value of the output.
+ * @param xMaximum The maximum viable value of the output.
+ * @return The output value after applying anti-windup.
+ */
+float pidApplyAntiWindup (pidController_t* pid, float xMinimum, float xMaximum);
 
 #endif // PID_CONTROLLER_H
