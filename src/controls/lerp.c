7653003ccc6d96f956a9bc82e17e99435612d29b
Cole Barach <colebarach@tutanota.com>
2025-02-08 13:21:29 -0500

    Added an implementation of bilinear interpolation. This interpolation is used to approximate the z-coordinate of a point based on its x,y coordinates within a quad.

--- src/controls/lerp.c
+++ src/controls/lerp.c
@@ -3,16 +3,59 @@
 
 float lerp (float x, float a, float b)
 {
+	// Scale X by range, then offset by min.
 	return x * (b - a) + a;
 }
 
 float inverseLerp (float x, float a, float b)
 {
+	// Prevent division-by-zero. If both points are identical, this is the most useful answer.
+	if (b == a)
+		return 0;
+
+	// Offset x by min, then divide by range.
 	return (x - a) / (b - a);
 }
 
-float lerp2d (float x, float xa, float ya, float xb, float yb)
+float lerp2d (float cx, float ax, float ay, float bx, float by)
 {
+	// Prevent division-by-zero. If both points are identical, this is the most useful answer.
+	if (bx == ax)
+		return ay;
+
 	// Perform inverse lerp then lerp.
-	return (x - xa) / (xb - xa) * (yb - ya) + ya;
+	return (cx - ax) / (bx - ax) * (by - ay) + ay;
+}
+
+float bilinearInterpolation (float x3, float y3, float x1, float y1, float x2, float y2,
+	float z11, float z12, float z21, float z22)
+{
+	// The X-Y plane looks something like this, keep in mind it's 3D.
+	//
+	//  Y
+	//  |  Q12    Y-Z        Q22
+	// y2   o------o----------o  2nd X-Z
+	//  |   |      |          |
+	//  |   |      |          |
+	// y3   |      o          |
+	//  |   |      |          |
+	//  |   |      |          |
+	//  |   |      |          |
+	// y1   o------o----------o  1st X-Z
+	//  |  Q11               Q21
+	//  |
+	//  0---x1-----x3---------x2--- X
+
+	// Normalize the input coordinates (x1,y1 => 0, x2,y2 => 1)
+	float x = inverseLerp (x3, x1, x2);
+	float y = inverseLerp (y3, y1, y2);
+
+	// Lerp the 1st X-Z plane
+	float z1 = lerp (x, z11, z12);
+
+	// Lerp the 2nd X-Z plane
+	float z2 = lerp (x, z21, z22);
+
+	// Lerp the central Y-Z plane
+	return lerp (y, z1, z2);
 }
--- src/controls/lerp.h
+++ src/controls/lerp.h
@@ -20,15 +20,32 @@ float lerp (float x, float a, float b);
 float inverseLerp (float x, float a, float b);
 
 /**
- * @brief Performs 2D linear interpolation from the the points (xa, ya) and (xb, yb).
+ * @brief Performs 2D linear interpolation from the the points A and B.
  * @note This can be used for inverse 2D linear interpolation by swapping the x and y coordinates.
- * @param x The input point's x-coordinate.
- * @param xa Point A's x-coordinate.
- * @param ya Point A's y-coordinate.
- * @param xb Point B's x-coordinate.
- * @param yb Point B's y-coordinate.
+ * @param cx The input point's x-coordinate.
+ * @param ax Point A's x-coordinate.
+ * @param ay Point A's y-coordinate.
+ * @param bx Point B's x-coordinate.
+ * @param by Point B's y-coordinate.
  * @return The y-coordinate of the interpolated point.
  */
-float lerp2d (float x, float xa, float ya, float xb, float yb);
+float lerp2d (float cx, float ax, float ay, float bx, float by);
+
+/**
+ * @brief Performs 3D bilinear interpolation on between the points Q11, Q12, Q21, and Q22.
+ * @param x3 The input point's x-coordinate.
+ * @param y3 The input point's y-coordinate.
+ * @param x1 The x-coordinate of points Q11 and Q12.
+ * @param y1 The y-coordinate of points Q11 and Q21.
+ * @param x2 The x-coordinate of points Q21 and Q22.
+ * @param y2 The y-coordinate of points Q12 and Q22.
+ * @param z11 The z-coordinate of point Q11.
+ * @param z12 The z-coordinate of point Q12.
+ * @param z21 The z-coordinate of point Q21.
+ * @param z22 The z-coordinate of point Q22.
+ * @return The z-coordinate of the interpolated point.
+ */
+float bilinearInterpolation (float x3, float y3, float x1, float y1, float x2, float y2,
+	float z11, float z12, float z21, float z22);
 
 #endif // LERP_H
