b210b5d194f03a8152c79ccac9061f3cbbcc69da
Cole Barach <colebarach@tutanota.com>
2025-09-17 10:10:19 -0400

    Fixed the compile_commands.json target using the Cygwin path instead of the native Windows path on Windows. With this, Clangd should now work correctly on Windows.

--- make/clangd.mk
+++ make/clangd.mk
@@ -1,5 +1,14 @@
 CLANGD_FILE := $(BUILDDIR)/compile_commands.json
 
+# On Windows, the PWD command needs passed through cygpath to convert into a native Windows path.
+ifeq ($(OS),Windows_NT)
+	# Windows_NT on XP, 2000, 7, Vista, 10...
+	PWD_CMD := $$(cygpath -w $$PWD)
+else
+	# POSIX, Linux, etc.
+	PWD_CMD := $$PWD
+endif
+
 # This generates the Clangd compile_commands.json file.
 $(CLANGD_FILE): $(CSRC) $(ALLINC)
 	rm -f $(BUILDDIR)/compile_commands.json;
@@ -7,7 +16,7 @@ $(CLANGD_FILE): $(CSRC) $(ALLINC)
 	printf "[\n"													>> $(CLANGD_FILE);
 	for c in $(CSRC); do \
 		printf "\t{\n"												>> $(CLANGD_FILE); \
-		printf "\t\t\"directory\": \"$(CURDIR)/\",\n"				>> $(CLANGD_FILE); \
+		printf "\t\t\"directory\": \"%q\",\n" $(PWD_CMD)			>> $(CLANGD_FILE); \
 		printf "\t\t\"command\": \"arm-none-eabi-gcc "				>> $(CLANGD_FILE); \
 		printf "%s" "--std=c99 $(USE_OPT)"							>> $(CLANGD_FILE); \
 		for i in $(ALLINC); do \
