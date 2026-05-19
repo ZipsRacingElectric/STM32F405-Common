b0ef5a6efe7fe6fd1ff78eb525ffb9a0388b0abc
Cole Barach <colebarach@tutanota.com>
2026-05-19 09:33:54 -0400

    Added a new analog sensor representing a thermistor modeled by the Beta Parameter Equation. Renamed the existing thermistor datatype to distinguish it (based on the Steinhart-Hart model). There is (theoretically) no functionality change here.

--- src/peripherals/adc/thermistor_pulldown.c
+++ src/peripherals/adc/thermistor_pulldown.c
@@ -4,24 +4,73 @@
 // Includes
 #include "controls/steinhart_hart.h"
 
+// C Standard Library
+#include <math.h>
+
 // Function Prototypes --------------------------------------------------------------------------------------------------------
 
 /**
- * @brief Updates the temperature of a thermistor.
+ * @brief Updates the temperature of a Steinhart-Hart thermistor.
  * @note This function uses a @c void* for the object reference as to make the signature usable by callbacks.
- * @param object The thermistor to update (must be a @c thermistorPulldown_t* ).
+ * @param object The thermistor to update (must be a @c thermistorSteinhartHartPulldown_t* ).
  * @param sample The read sample.
  * @param sampleVdd The sample of the analog supply voltage.
  */
-static void callback (void* object, uint16_t sample, uint16_t sampleVdd);
+static void steinhartHartCallback (void* object, uint16_t sample, uint16_t sampleVdd);
+
+/**
+ * @brief Updates the temperature of a Beta Model thermistor.
+ * @note This function uses a @c void* for the object reference as to make the signature usable by callbacks.
+ * @param object The thermistor to update (must be a @c thermistorBetaPulldown_t* ).
+ * @param sample The read sample.
+ * @param sampleVdd The sample of the analog supply voltage.
+ */
+static void betaCallback (void* object, uint16_t sample, uint16_t sampleVdd);
 
 // Functions ------------------------------------------------------------------------------------------------------------------
 
-bool thermistorPulldownInit (thermistorPulldown_t* thermistor, const thermistorPulldownConfig_t* config)
+/**
+ * @brief Gets the resistance of a thermistor from the output sample and the VDD sample.
+ * @param sample The output sample.
+ * @param sampleVdd The sample of the VDD voltage supply.
+ * @param resistancePullup The resistance of the pullup resistor.
+ * @return The calculated resistance, in Ohms.
+ */
+static float sampleToResistance (uint16_t sample, uint16_t sampleVdd, float resistancePullup)
+{
+	// Circuit Analysis:
+	//   V_ADC (R_Therm) = R_Therm * V_VDD / (R_Pullup + R_Therm)
+	//   R_Therm (V_ADC) = V_ADC * R_Pullup / (V_VDD - V_ADC)
+	//
+	//   X_ADC (V_ADC) = X_VDD * V_ADC / V_VDD
+	//   V_ADC (X_ADC) = X_ADC * V_VDD / X_VDD
+	//
+	//   R_Therm (X_ADC) = X_ADC * V_VDD / X_VDD * R_Pullup / (V_VDD - X_ADC * V_VDD / X_VDD)
+	//                   = X_ADC * R_Pullup / (X_VDD * (1 - X_ADC/X_VDD))
+	//                   = X_ADC * R_Pullup / (X_VDD - X_ADC)
+
+	return sample * resistancePullup / (sampleVdd - sample);
+}
+
+/**
+ * @brief Converts a thermistor's resistance to a temperature based on the Beta Parameter Equation.
+ * @param resistance The measured resistance.
+ * @param beta The beta parameter, in degrees Kelvin.
+ * @param referenceResistance The reference resistance (resistance at reference temperature), in Ohms.
+ * @param referenceTemperature The reference temperature (ex. 25C), in degrees Celsius.
+ * @return The calculated temperature, in degrees Celsius.
+ */
+static float betaTemperature (float resistance, float beta, float referenceResistance, float referenceTemperature)
+{
+	// Refactored version of the equation provided from: https://www.ametherm.com/thermistor/ntc-thermistor-beta.
+	return 1 / (1 / beta * logf (resistance / referenceResistance) + 1 / (referenceTemperature + 273.15f)) - 273.15f;
+}
+
+bool thermistorSteinhartHartPulldownInit (thermistorSteinhartHartPulldown_t* thermistor, const thermistorSteinhartHartPulldownConfig_t* config)
 {
 	// Store the configuration
 	thermistor->config = config;
-	thermistor->callback = callback;
+	thermistor->callback = steinhartHartCallback;
 
 	// Validate the configuration
 	if (config->temperatureMin >= config->temperatureMax)
@@ -36,9 +85,9 @@ bool thermistorPulldownInit (thermistorPulldown_t* thermistor, const thermistorP
 	return thermistor->state != ANALOG_SENSOR_CONFIG_INVALID;
 }
 
-void callback (void* object, uint16_t sample, uint16_t sampleVdd)
+void steinhartHartCallback (void* object, uint16_t sample, uint16_t sampleVdd)
 {
-	thermistorPulldown_t* thermistor = (thermistorPulldown_t*) object;
+	thermistorSteinhartHartPulldown_t* thermistor = (thermistorSteinhartHartPulldown_t*) object;
 
 	// Store the sample.
 	thermistor->sample = sample;
@@ -65,22 +114,72 @@ void callback (void* object, uint16_t sample, uint16_t sampleVdd)
 
 	thermistor->state = ANALOG_SENSOR_VALID;
 
-	// Circuit Analysis:
-	//   V_ADC (R_Therm) = R_Therm * V_VDD / (R_Pullup + R_Therm)
-	//   R_Therm (V_ADC) = V_ADC * R_Pullup / (V_VDD - V_ADC)
-	//
-	//   X_ADC (V_ADC) = X_VDD * V_ADC / V_VDD
-	//   V_ADC (X_ADC) = X_ADC * V_VDD / X_VDD
-	//
-	//   R_Therm (X_ADC) = X_ADC * V_VDD / X_VDD * R_Pullup / (V_VDD - X_ADC * V_VDD / X_VDD)
-	//                   = X_ADC * R_Pullup / (X_VDD * (1 - X_ADC/X_VDD))
-	//                   = X_ADC * R_Pullup / (X_VDD - X_ADC)
+	// Calculate the resistance and temperature from the given information
 
-	thermistor->resistance = sample * thermistor->config->resistancePullup / (sampleVdd - sample);
+	thermistor->resistance = sampleToResistance (sample, sampleVdd, thermistor->config->resistancePullup);
 
 	thermistor->temperature = steinhartHartTemperature (thermistor->resistance, thermistor->config->resistanceReference,
 		thermistor->config->steinhartHartA, thermistor->config->steinhartHartB, thermistor->config->steinhartHartC,
 		thermistor->config->steinhartHartD);
+
+	thermistor->overtemperatureFault = thermistor->temperature > thermistor->config->temperatureMax;
+	thermistor->undertemperatureFault = thermistor->temperature < thermistor->config->temperatureMin;
+}
+
+bool thermistorBetaPulldownInit (thermistorBetaPulldown_t* thermistor, const thermistorBetaPulldownConfig_t* config)
+{
+	// Store the configuration
+	thermistor->config = config;
+	thermistor->callback = betaCallback;
+
+	// Validate the configuration
+	if (config->temperatureMin >= config->temperatureMax)
+		thermistor->state = ANALOG_SENSOR_CONFIG_INVALID;
+	else
+	 	thermistor->state = ANALOG_SENSOR_SAMPLE_INVALID;
+
+	// Set values to their defaults
+	thermistor->temperature = 0.0f;
+	thermistor->sample = 0;
+
+	return thermistor->state != ANALOG_SENSOR_CONFIG_INVALID;
+}
+
+void betaCallback (void* object, uint16_t sample, uint16_t sampleVdd)
+{
+	thermistorBetaPulldown_t* thermistor = (thermistorBetaPulldown_t*) object;
+
+	// Store the sample.
+	thermistor->sample = sample;
+
+	// If the peripheral has failed or the config is invalid, don't check anything else.
+	if (thermistor->state == ANALOG_SENSOR_CONFIG_INVALID || thermistor->state == ANALOG_SENSOR_FAILED)
+		return;
+
+	// Check the sample is in the valid range
+	if (sample >= sampleVdd)
+	{
+		thermistor->state = ANALOG_SENSOR_SAMPLE_INVALID;
+		thermistor->undertemperatureFault = true;
+		thermistor->temperature = thermistor->config->temperatureMin;
+		return;
+	}
+	else if (sample == 0)
+	{
+		thermistor->state = ANALOG_SENSOR_SAMPLE_INVALID;
+		thermistor->overtemperatureFault = true;
+		thermistor->temperature = thermistor->config->temperatureMax;
+		return;
+	}
+
+	thermistor->state = ANALOG_SENSOR_VALID;
+
+	// Calculate the resistance and temperature from the given information
+
+	thermistor->resistance = sampleToResistance (sample, sampleVdd, thermistor->config->resistancePullup);
+
+	thermistor->temperature = betaTemperature (thermistor->resistance, thermistor->config->beta,
+		thermistor->config->referenceResistance, thermistor->config->referenceTemperature);
 
 	thermistor->overtemperatureFault = thermistor->temperature > thermistor->config->temperatureMax;
 	thermistor->undertemperatureFault = thermistor->temperature < thermistor->config->temperatureMin;
--- src/peripherals/adc/thermistor_pulldown.h
+++ src/peripherals/adc/thermistor_pulldown.h
@@ -6,7 +6,7 @@
 // Author: Cole Barach
 // Date Created: 2025.04.04
 //
-// Description: Analog sensor representing a thermistor connected as the pulldown resistor of a resistor divider.
+// Description: Analog sensors representing a thermistor connected as the pulldown resistor of a resistor divider.
 //
 // Circuit Schematic:
 //    VDD
@@ -14,7 +14,7 @@
 //     |
 //   -----
 //   |   |
-//   | R |
+//   | R | Pullup resistance (constant)
 //   |   |
 //   -----
 //     |
@@ -22,12 +22,16 @@
 //     |
 //   -----
 //   |   |
-//   | T |
+//   | T | Thermistor resistance (variable)
 //   |   |
 //   -----
 //     |
 //    ---
 //    GND
+//
+// Note that there are multiple ways of converting from a thermistor's resistance to a temperature. These different models of
+//   conversion use different parameters to characterize the thermistor, and therefore use different configurations. Depending
+//   on what information is provided in the datasheet, different models can be used here.
 
 // Includes -------------------------------------------------------------------------------------------------------------------
 
@@ -36,6 +40,7 @@
 
 // Datatypes ------------------------------------------------------------------------------------------------------------------
 
+/// @brief Config for the @c thermistorSteinhartHartPulldown_t analog sensor.
 typedef struct
 {
 	/// @brief The A coefficient of the Steinhart-Hart equation.
@@ -50,7 +55,7 @@ typedef struct
 	/// @brief The D coefficient of the Steinhart-Hart equation.
 	float steinhartHartD;
 
-	/// @brief The reference resistance of the Steinhart-Hart equation.
+	/// @brief The reference resistance of the Steinhart-Hart equation, in Ohms.
 	float resistanceReference;
 
 	/// @brief The resistance of the circuit's pullup resistor, in Ohms.
@@ -61,21 +66,61 @@ typedef struct
 
 	/// @brief The maximum plausible temperature, used to detect short-circuit faults.
 	float temperatureMax;
-} thermistorPulldownConfig_t;
+} thermistorSteinhartHartPulldownConfig_t;
 
 /**
  * @brief Datatype representing a thermistor connected in a resistor divider.
+ * @note This thermistor uses the Steinhart-Hart model for temperature conversion. See below for more info:
+ * https://en.wikipedia.org/wiki/Steinhart%E2%80%93Hart_equation
  */
 typedef struct
 {
 	ANALOG_SENSOR_FIELDS;
-	const thermistorPulldownConfig_t* config;
+	const thermistorSteinhartHartPulldownConfig_t* config;
 	uint16_t sample;
 	float resistance;
 	float temperature;
 	bool undertemperatureFault;
 	bool overtemperatureFault;
-} thermistorPulldown_t;
+} thermistorSteinhartHartPulldown_t;
+
+/// @brief Config for the @c thermistorBetaPulldown_t analog sensor.
+typedef struct
+{
+	/// @brief The beta coefficient of the thermistor's Beta Parameter Equation (in degrees Kelvin).
+	float beta;
+
+	/// @brief The resistance of the thermistor at a known temperature (the reference temperature), in Ohms.
+	float referenceResistance;
+
+	/// @brief The temperature of the thermistor at a known resistance (the reference resistance), in degrees Celsius.
+	float referenceTemperature;
+
+	/// @brief The resistance of the circuit's pullup resistor, in Ohms.
+	float resistancePullup;
+
+	/// @brief The minimum plausible temperature, used to detect open-circuit faults.
+	float temperatureMin;
+
+	/// @brief The maximum plausible temperature, used to detect short-circuit faults.
+	float temperatureMax;
+} thermistorBetaPulldownConfig_t;
+
+/**
+ * @brief Datatype representing a thermistor connected in a resistor divider.
+ * @note This thermistor uses the Beta Parameter Equation for temperature conversion. See below for more info:
+ * https://www.ametherm.com/thermistor/ntc-thermistor-beta
+ */
+typedef struct
+{
+	ANALOG_SENSOR_FIELDS;
+	const thermistorBetaPulldownConfig_t* config;
+	uint16_t sample;
+	float resistance;
+	float temperature;
+	bool undertemperatureFault;
+	bool overtemperatureFault;
+} thermistorBetaPulldown_t;
 
 // Functions ------------------------------------------------------------------------------------------------------------------
 
@@ -85,6 +130,15 @@ typedef struct
  * @param config The configuration to use.
  * @return True if successful, false otherwise.
  */
-bool thermistorPulldownInit (thermistorPulldown_t* thermistor, const thermistorPulldownConfig_t* config);
+bool thermistorSteinhartHartPulldownInit (thermistorSteinhartHartPulldown_t* thermistor,
+	const thermistorSteinhartHartPulldownConfig_t* config);
+
+/**
+ * @brief Initializes the thermistor using the specified configuration.
+ * @param thermistor The thermistor to initialize.
+ * @param config The configuration to use.
+ * @return True if successful, false otherwise.
+ */
+bool thermistorBetaPulldownInit (thermistorBetaPulldown_t* thermistor, const thermistorBetaPulldownConfig_t* config);
 
 #endif // THERMISTOR_PULLDOWN_H
