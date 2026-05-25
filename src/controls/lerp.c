a08de9a037ae35d20f4b077d1526aacfccd67a84
Cole Barach <colebarach@tutanota.com>
2026-05-25 13:08:06 -0400

    Added new lerp functions to be used by the VCU. This module has gotten somewhat out-of-hand, but I believe this is still the best place for all of this.

--- src/controls/lerp.c
+++ src/controls/lerp.c
@@ -2,7 +2,7 @@
 #include "lerp.h"
 
 // C Standard Library
-#include <stdbool.h>
+#include <math.h>
 
 float lerp (float x, float a, float b)
 {
@@ -50,22 +50,6 @@ float lerp2dSaturated (float cx, float ax, float ay, float bx, float by)
 float bilinearInterpolation (float x3, float y3, float x1, float y1, float x2, float y2,
 	float z11, float z12, float z21, float z22)
 {
-	// The X-Y plane looks something like this, keep in mind it's 3D.
-	//
-	//  Y
-	//  |  Q12    Y-Z        Q22
-	// y2   o------o----------o  2nd X-Z
-	//  |   |      |          |
-	//  |   |      |          |
-	// y3   |      o          |
-	//  |   |      |          |
-	//  |   |      |          |
-	//  |   |      |          |
-	// y1   o------o----------o  1st X-Z
-	//  |  Q11               Q21
-	//  |
-	//  0---x1-----x3---------x2--- X
-
 	// Normalize the input coordinates (x1,y1 => 0, x2,y2 => 1)
 	float x = inverseLerp (x3, x1, x2);
 	float y = inverseLerp (y3, y1, y2);
@@ -78,4 +62,89 @@ float bilinearInterpolation (float x3, float y3, float x1, float y1, float x2, f
 
 	// Lerp the central Y-Z plane
 	return lerp (y, z1, z2);
+}
+
+float bilinearLookupTable (float x, float y, float xMin, float xMax, float yMin, float yMax,
+	const float* table, uint16_t xWidth, uint16_t yWidth)
+{
+	// Get the floating-point x & y indices
+	float x3 = lerp2dSaturated (x, xMin, 0, xMax, xWidth - 1);
+	float y3 = lerp2dSaturated (y, yMin, 0, yMax, yWidth - 1);
+
+	// Get the upper and lower bounds on the x & y indices
+	uint8_t x1 = floorf (x3);
+	uint8_t x2 = ceilf  (x3);
+	uint8_t y1 = floorf (y3);
+	uint8_t y2 = ceilf  (y3);
+
+	// Get the 4 points to interpolate at.
+	float z11 = table [x1 + y1 * xWidth];
+	float z12 = table [x1 + y2 * xWidth];
+	float z21 = table [x2 + y1 * xWidth];
+	float z22 = table [x2 + y2 * xWidth];
+
+	// Perform bilinear interpolation between the 4 points.
+	return bilinearInterpolation (x3, y3, x1, y1, x2, y2, z11, z12, z21, z22);
+}
+
+float inverseLerpHysteresis (float x, float a, float b, float hyst, bool* increasingCurve, float* cPrime)
+{
+	if (x < a)
+	{
+		// If before the start of the interpolation, return 0.
+		*increasingCurve = true;
+		*cPrime = 0;
+		return 0;
+	}
+	else if (x > b)
+	{
+		// If after the end of interpolation, return 1.
+		*increasingCurve = false;
+		*cPrime = 1;
+		return 1;
+	}
+	else
+	{
+		// Compute the outputs for both the increasing and decreasing curves
+		float cIncreasing = (x - a - hyst) / (b - a - hyst);
+		float cDecreasing = (x - a) / (b - hyst - a);
+
+		if (*increasingCurve)
+		{
+			if (cIncreasing > *cPrime)
+			{
+				// If on the increasing curve and the output has increased, return said output.
+				*cPrime = cIncreasing;
+				return cIncreasing;
+			}
+			else if (cDecreasing < *cPrime)
+			{
+				// If on the increasing curve and the input has decreased enough to hit the decreasing curve, switch curves
+				// and return said output.
+				*increasingCurve = false;
+				*cPrime = cDecreasing;
+				return cDecreasing;
+			}
+		}
+		else
+		{
+			if (cDecreasing < *cPrime)
+			{
+				// If on the decreasing curve and the output has decreased, return said output.
+				*cPrime = cDecreasing;
+				return cDecreasing;
+			}
+			else if (cIncreasing > *cPrime)
+			{
+				// If on the decreasing curve and the input has increased enough to hit the increasing curve, switch curves
+				// and return said output.
+				*increasingCurve = true;
+				*cPrime = cIncreasing;
+				return cIncreasing;
+			}
+		}
+	}
+
+	// If the output has not changed, return the last value.
+	return *cPrime;
 }
--- src/controls/lerp.h
+++ src/controls/lerp.h
@@ -8,6 +8,12 @@
 //
 // Description: Group of functions related to linear interpolation.
 
+// Includes -------------------------------------------------------------------------------------------------------------------
+
+// C Standard Library
+#include <stdint.h>
+#include <stdbool.h>
+
 // Functions ------------------------------------------------------------------------------------------------------------------
 
 /**
@@ -59,8 +65,69 @@ float lerp2dSaturated (float cx, float ax, float ay, float bx, float by);
  * @param z21 The z-coordinate of point Q21.
  * @param z22 The z-coordinate of point Q22.
  * @return The z-coordinate of the interpolated point.
+ *
+ * @note The X-Y plane looks something like this, keep in mind it's 3D:
+ *
+ *  Y
+ *  |  Q12    Y-Z        Q22
+ * y2   o------o----------o  2nd X-Z
+ *  |   |      |          |
+ *  |   |      |          |
+ * y3   |      o          |
+ *  |   |      |          |
+ *  |   |      |          |
+ *  |   |      |          |
+ * y1   o------o----------o  1st X-Z
+ *  |  Q11               Q21
+ *  |
+ *  0---x1-----x3---------x2--- X
+ *
  */
 float bilinearInterpolation (float x3, float y3, float x1, float y1, float x2, float y2,
 	float z11, float z12, float z21, float z22);
+
+/**
+ * @brief Performs bilinear interpolation on a loopup table.
+ * @param x The x value to interpolate for.
+ * @param y The y value to interpolate for.
+ * @param xMin The minimum value of the x-axis (maps to index @c 0 ).
+ * @param xMax The maximum value of the x-axis (maps to index @c xWidth-1 )
+ * @param yMin The maximum value of the y-axis (maps to index @c 0 ).
+ * @param yMax The maximum value of the y-axis (maps to index @c xWidth-1 )
+ * @param table The 2D array to pull data from. Of size [ @c xWidth ][ @c yWidth ].
+ * @param xWidth The width of the table along the x-axis.
+ * @param yWidth The width of the table along the y-axis.
+ * @return The interpolated value.
+ */
+float bilinearLookupTable (float x, float y, float xMin, float xMax, float yMin, float yMax,
+	const float* table, uint16_t xWidth, uint16_t yWidth);
+
+/**
+ * @brief Performs inverse linear interpolation, with hysteresis applied to the input. This means that brief deviations in the
+ * direction of the input will not affect the output. Reference the below graph for details.
+ *
+ * Output (C)
+ *   |
+ * 1 o                 /-<----<-/-<---<-----
+ *   |                /        /
+ *   |    Decreasing /        / Increasing
+ *   |        Curve /        / Curve
+ *   |             /        /
+ * 0 o ----->--->-/->---->-/
+ *   |
+ *   o------------o--------o---o------------- Input (X)
+ *                A            B
+ *                |--hyst--|
+ *
+ * @param x The point to interpolate. In the same units as A and B.
+ * @param a The point to start interpolation at. When decreasing, this is the point at which the output will hit 0.
+ * @param b The point to end interpolation at. When increasing, this is the point at which the output will hit 1.
+ * @param hyst The amount of hysteresis to use. This defines the maximum amount of deviation in the input value before the
+ * output direction changes.
+ * @param increasingCurve Buffer to write the increasing/decreasing state into.
+ * @param cPrime Buffer to write the last value of C into.
+ * @return The output, C. Always in the range [0, 1].
+ */
+float inverseLerpHysteresis (float x, float a, float b, float hyst, bool* increasingCurve, float* cPrime);
 
 #endif // LERP_H
